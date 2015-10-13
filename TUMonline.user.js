94bb5842b7d599ba5f1b543fe8a7624e55214941
Michael Engelhard <engelhard.michael+git@gmail.com>
2015-10-13 23:50:21 +0200

    Fixed a bug that prevented some javascript powered popups on the same page from being opened. Added updateURL metadata.

--- TUMonline.user.js
+++ TUMonline.user.js
@@ -1,14 +1,21 @@
 ﻿// ==UserScript==
 // @name         TUMonline
 // @namespace    mihi314
-// @version      1.1
+// @version      1.2
+// @updateURL    https://github.com/mihi314/tumonline/raw/master/TUMonline.user.js
 // @description  don't open every goddamned link in a new window
 // @match        https://campus.tum.de/tumonline/*
 // @grant        none
 // ==/UserScript==
 
 if (window.CO) {
-    window.CO.WIN = function() {return true;}
+    WIN_ORIG = window.CO.WIN
+    window.CO.WIN = function(pAnchor, pType, pCustom) {
+        // don't do anything when called with more than one argument
+        if (pType === undefined)
+            return true
+        return WIN_ORIG(pAnchor, pType, pCustom)
+    }
 }
 
 if (window.GLOBALnarrowWindow) {
